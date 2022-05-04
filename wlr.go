fa9e8c9725b1298fc925ab707309a62c1d6d24f3
DeedleFake <deedlefake@users.noreply.github.com>
2022-05-04 15:46:24 -0400

    wlr: refactor `Renderer` into its own file

--- wlr.go
+++ wlr.go
@@ -12,8 +12,6 @@ package wlr
 // #include <wlr/backend.h>
 // #include <wlr/backend/wayland.h>
 // #include <wlr/backend/x11.h>
-// #include <wlr/render/wlr_renderer.h>
-// #include <wlr/render/wlr_texture.h>
 // #include <wlr/util/box.h>
 // #include <wlr/types/wlr_compositor.h>
 // #include <wlr/types/wlr_cursor.h>
@@ -33,7 +31,7 @@ package wlr
 // #include <wlr/util/log.h>
 // #include <wlr/xwayland.h>
 //
-// #include "wlroots.h"
+// #include "wlr.h"
 import "C"
 
 import (
@@ -520,55 +518,6 @@ func (s SeatPointerState) FocusedSurface() Surface {
 
 func (s SeatPointerState) FocusedClient() SeatClient {
 	return SeatClient{p: s.s.focused_client}
-}
-
-type Renderer struct {
-	p *C.struct_wlr_renderer
-}
-
-func NewRenderer(backend Backend) Renderer {
-	p := C.wlr_renderer_autocreate(backend.p)
-	man.track(unsafe.Pointer(p), &p.events.destroy)
-	return Renderer{p: p}
-}
-
-func (r Renderer) Destroy() {
-	C.wlr_renderer_destroy(r.p)
-}
-
-func (r Renderer) OnDestroy(cb func(Renderer)) {
-	man.add(unsafe.Pointer(r.p), &r.p.events.destroy, func(unsafe.Pointer) {
-		cb(r)
-	})
-}
-
-func (r Renderer) InitDisplay(display Display) {
-	C.wlr_renderer_init_wl_display(r.p, display.p)
-}
-
-func (r Renderer) Begin(output Output, width int, height int) {
-	C.wlr_renderer_begin(r.p, C.uint(width), C.uint(height))
-}
-
-func (r Renderer) Clear(color *Color) {
-	c := color.toC()
-	C.wlr_renderer_clear(r.p, &c[0])
-}
-
-func (r Renderer) End() {
-	C.wlr_renderer_end(r.p)
-}
-
-func (r Renderer) RenderTextureWithMatrix(texture Texture, matrix *Matrix, alpha float32) {
-	m := matrix.toC()
-	C.wlr_render_texture_with_matrix(r.p, texture.p, &m[0], C.float(alpha))
-}
-
-func (r Renderer) RenderRect(box *Box, color *Color, projection *Matrix) {
-	b := box.toC()
-	c := color.toC()
-	pm := projection.toC()
-	C.wlr_render_rect(r.p, &b, &c[0], &pm[0])
 }
 
 type Output struct {
