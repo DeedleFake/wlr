949659bf6d3ea6652ae4690883d4645bbdfda3c6
DeedleFake <deedlefake@users.noreply.github.com>
2022-05-04 15:43:11 -0400

    wlr: begin refactoring into separate files

--- wlr.go
+++ wlr.go
@@ -1,16 +1,20 @@
 package wlr
 
+// #cgo pkg-config: wlroots wayland-server
+// #cgo CFLAGS: -D_GNU_SOURCE -DWLR_USE_UNSTABLE
+//
 // #include <stdarg.h>
 // #include <stdio.h>
 // #include <stdlib.h>
 // #include <time.h>
 // #include <wayland-server.h>
+//
 // #include <wlr/backend.h>
 // #include <wlr/backend/wayland.h>
 // #include <wlr/backend/x11.h>
 // #include <wlr/render/wlr_renderer.h>
 // #include <wlr/render/wlr_texture.h>
-// #include <wlr/types/wlr_box.h>
+// #include <wlr/util/box.h>
 // #include <wlr/types/wlr_compositor.h>
 // #include <wlr/types/wlr_cursor.h>
 // #include <wlr/types/wlr_data_device.h>
@@ -29,36 +33,7 @@ package wlr
 // #include <wlr/util/log.h>
 // #include <wlr/xwayland.h>
 //
-// void _wlr_log_cb(enum wlr_log_importance importance, char *msg);
-//
-// static inline void _wlr_log_inner_cb(enum wlr_log_importance importance, const char *fmt, va_list args) {
-//		char *msg = NULL;
-//		if (vasprintf(&msg, fmt, args) == -1) {
-//			return;
-//		}
-//
-//		_wlr_log_cb(importance, msg);
-//		free(msg);
-// }
-//
-// static inline void _wlr_log_set_cb(enum wlr_log_importance verbosity, bool is_set) {
-//		wlr_log_init(verbosity, is_set ? &_wlr_log_inner_cb : NULL);
-// }
-//
-// void _wlr_xdg_surface_for_each_cb(struct wlr_surface *surface, int sx, int sy, void *data);
-//
-// static inline void _wlr_xdg_surface_for_each_surface(struct wlr_xdg_surface *surface, void *user_data) {
-//		wlr_xdg_surface_for_each_surface(surface, &_wlr_xdg_surface_for_each_cb, user_data);
-// }
-//
-// void _wl_listener_cb(struct wl_listener *listener, void *data);
-//
-// static inline void _wl_listener_set_cb(struct wl_listener *listener) {
-//		listener->notify = &_wl_listener_cb;
-// }
-//
-// #cgo pkg-config: wlroots wayland-server
-// #cgo CFLAGS: -D_GNU_SOURCE -DWLR_USE_UNSTABLE
+// #include "wlroots.h"
 import "C"
 
 import (
@@ -72,93 +47,6 @@ import (
 
 	"golang.org/x/sys/unix"
 )
-
-type XWayland struct {
-	p *C.struct_wlr_xwayland
-}
-
-type XWaylandSurface struct {
-	p *C.struct_wlr_xwayland_surface
-}
-
-func NewXWayland(display Display, compositor Compositor, lazy bool) XWayland {
-	p := C.wlr_xwayland_create(display.p, compositor.p, C.bool(lazy))
-	return XWayland{p: p}
-}
-
-func (x XWayland) Destroy() {
-	C.wlr_xwayland_destroy(x.p)
-}
-
-func (x XWayland) OnNewSurface(cb func(XWaylandSurface)) {
-	man.add(unsafe.Pointer(x.p), &x.p.events.new_surface, func(data unsafe.Pointer) {
-		surface := XWaylandSurface{p: (*C.struct_wlr_xwayland_surface)(data)}
-		man.track(unsafe.Pointer(surface.p), &surface.p.events.destroy)
-		man.add(unsafe.Pointer(surface.p.surface), &surface.p.surface.events.destroy, func(data unsafe.Pointer) {
-			man.delete(unsafe.Pointer(surface.p.surface))
-		})
-		cb(surface)
-	})
-}
-
-func (x XWayland) SetCursor(img XCursorImage) {
-	C.wlr_xwayland_set_cursor(x.p, img.p.buffer, img.p.width*4, img.p.width, img.p.height, C.int32_t(img.p.hotspot_x), C.int32_t(img.p.hotspot_y))
-}
-
-func (s XWaylandSurface) Surface() Surface {
-	return Surface{p: s.p.surface}
-}
-
-func (s XWaylandSurface) Geometry() Box {
-	return Box{
-		X:      int(s.p.x),
-		Y:      int(s.p.y),
-		Width:  int(s.p.width),
-		Height: int(s.p.height),
-	}
-}
-
-func (s XWaylandSurface) Configure(x int16, y int16, width uint16, height uint16) {
-	C.wlr_xwayland_surface_configure(s.p, C.int16_t(x), C.int16_t(y), C.uint16_t(width), C.uint16_t(height))
-}
-
-func (s XWaylandSurface) OnMap(cb func(XWaylandSurface)) {
-	man.add(unsafe.Pointer(s.p), &s.p.events._map, func(data unsafe.Pointer) {
-		cb(s)
-	})
-}
-
-func (s XWaylandSurface) OnUnmap(cb func(XWaylandSurface)) {
-	man.add(unsafe.Pointer(s.p), &s.p.events.unmap, func(data unsafe.Pointer) {
-		cb(s)
-	})
-}
-
-func (s XWaylandSurface) OnDestroy(cb func(XWaylandSurface)) {
-	man.add(unsafe.Pointer(s.p), &s.p.events.destroy, func(data unsafe.Pointer) {
-		cb(s)
-	})
-}
-
-func (s XWaylandSurface) OnRequestMove(cb func(surface XWaylandSurface)) {
-	man.add(unsafe.Pointer(s.p), &s.p.events.request_move, func(data unsafe.Pointer) {
-		cb(s)
-	})
-}
-
-func (s XWaylandSurface) OnRequestResize(cb func(surface XWaylandSurface, edges Edges)) {
-	man.add(unsafe.Pointer(s.p), &s.p.events.request_resize, func(data unsafe.Pointer) {
-		event := (*C.struct_wlr_xwayland_resize_event)(data)
-		cb(s, Edges(event.edges))
-	})
-}
-
-func (s XWaylandSurface) OnRequestConfigure(cb func(surface XWaylandSurface, x int16, y int16, width uint16, height uint16)) {
-	man.add(unsafe.Pointer(s.p), &s.p.events.request_configure, func(data unsafe.Pointer) {
-		event := (*C.struct_wlr_xwayland_surface_configure_event)(data)
-		cb(s, int16(event.x), int16(event.y), uint16(event.width), uint16(event.height))
-	})
-}
 
 type XDGSurfaceRole uint32
 
