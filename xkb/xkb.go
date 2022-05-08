9ec7a877b784eec144a9dfcd74e8b2a51d2b0364
DeedleFake <deedlefake@users.noreply.github.com>
2022-05-08 13:39:21 -0400

    xkb: add some missing stuff

--- xkb/xkb.go
+++ xkb/xkb.go
@@ -3,22 +3,22 @@ package xkb
 //go:generate go run ./internal/symsgen syms.go
 
 /*
+#cgo pkg-config: xkbcommon
+
 #include <stdlib.h>
 #include <xkbcommon/xkbcommon.h>
-#cgo pkg-config: xkbcommon
 */
 import "C"
 
 import "unsafe"
 
-type (
-	KeyCode     uint32
-	KeySymFlags uint32
-)
+type KeyCode uint32
+
+type KeySymFlags uint32
 
 const (
-	KeySymFlagNoFlags         KeySymFlags = C.XKB_KEYSYM_NO_FLAGS
-	KeySymFlagCaseInsensitive KeySymFlags = C.XKB_KEYSYM_CASE_INSENSITIVE
+	KeySymNoFlags         KeySymFlags = C.XKB_KEYSYM_NO_FLAGS
+	KeySymCaseInsensitive KeySymFlags = C.XKB_KEYSYM_CASE_INSENSITIVE
 )
 
 type Context struct {
@@ -29,6 +29,11 @@ type Keymap struct {
 	p *C.struct_xkb_keymap
 }
 
+func NewKeymapFromNames(ctx Context, rules *RuleNames, flags KeymapCompileFlags) Keymap {
+	p := C.xkb_keymap_new_from_names(ctx.p, rules.toC(), C.enum_xkb_keymap_compile_flags(flags))
+	return Keymap{p: p}
+}
+
 type State struct {
 	p *C.struct_xkb_state
 }
@@ -37,7 +42,7 @@ func WrapState(p unsafe.Pointer) State {
 	return State{p: (*C.struct_xkb_state)(p)}
 }
 
-func NewContext() Context {
+func NewContext(flags ContextFlags) Context {
 	p := C.xkb_context_new(C.XKB_CONTEXT_NO_FLAGS)
 	return Context{p: p}
 }
@@ -49,21 +54,15 @@ func SymFromName(name string, flags KeySymFlags) KeySym {
 	return KeySym(sym)
 }
 
-func (c Context) Destroy() {
+func (c Context) Unref() {
 	C.xkb_context_unref(c.p)
-}
-
-func (c Context) Map() Keymap {
-	var rules C.struct_xkb_rule_names
-	p := C.xkb_keymap_new_from_names(c.p, &rules, C.XKB_KEYMAP_COMPILE_NO_FLAGS)
-	return Keymap{p: p}
 }
 
 func (m Keymap) Ptr() unsafe.Pointer {
 	return unsafe.Pointer(m.p)
 }
 
-func (m Keymap) Destroy() {
+func (m Keymap) Unref() {
 	C.xkb_keymap_unref(m.p)
 }
 
@@ -82,3 +81,39 @@ func (s State) Syms(keyCode KeyCode) []KeySym {
 
 	return res
 }
+
+type RuleNames struct {
+	Rules   string
+	Model   string
+	Layout  string
+	Variant string
+	Options string
+}
+
+func (rn *RuleNames) toC() *C.struct_xkb_rule_names {
+	if rn == nil {
+		return nil
+	}
+
+	return &C.struct_xkb_rule_names{
+		rules:   C.CString(rn.Rules),
+		model:   C.CString(rn.Model),
+		layout:  C.CString(rn.Layout),
+		variant: C.CString(rn.Variant),
+		options: C.CString(rn.Options),
+	}
+}
+
+type ContextFlags int
+
+const (
+	ContextNoFlags            ContextFlags = C.XKB_CONTEXT_NO_FLAGS
+	ContextNoDefaultincludes               = C.XKB_CONTEXT_NO_DEFAULT_INCLUDES
+	ContextNoEnvironmentNames              = C.XKB_CONTEXT_NO_ENVIRONMENT_NAMES
+)
+
+type KeymapCompileFlags int
+
+const (
+	KeymapCompileNoFlags KeymapCompileFlags = C.XKB_KEYMAP_COMPILE_NO_FLAGS
+)
